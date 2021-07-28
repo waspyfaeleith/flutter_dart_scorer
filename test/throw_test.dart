d60fcd5238256a65b35e4c35dba34e4e821e4499
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2021-07-28 14:56:47 +0100

    tests for throw class added and passing

--- test/throw_test.dart
+++ test/throw_test.dart
@@ -8,5 +8,35 @@ void main() {
       expect(180, playerThrow.score);
     });
 
+    test('181 is not a valid score', () {
+      final Throw playerThrow = new Throw(181);
+      expect(false, playerThrow.isValid());
+    });
+
+    test('163 is not a valid score', () {
+      final Throw playerThrow = new Throw(163);
+      expect(false, playerThrow.isValid());
+    });
+
+    test('164 is a valid score', () {
+      final Throw playerThrow = new Throw(164);
+      expect(true, playerThrow.isValid());
+    });
+
+    test('100 is a valid score', () {
+      final Throw playerThrow = new Throw(100);
+      expect(true, playerThrow.isValid());
+    });
+
+    test('0 is a valid score', () {
+      final Throw playerThrow = new Throw(0);
+      expect(true, playerThrow.isValid());
+    });
+
+    test('negative value is not a valid score', () {
+      final Throw playerThrow = new Throw(-10);
+      expect(false, playerThrow.isValid());
+    });
+
   });
 }
